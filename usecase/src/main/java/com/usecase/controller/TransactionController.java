9e439e0c6a8d59caa2ca01cb9a81feae10b4b698
AyushmanS <AyushmanS@LP-R9030X42.HCLT.CORP.HCL.IN>
2019-02-15 11:48:15 +0530

    Transaction Error removed

--- usecase/src/main/java/com/usecase/controller/TransactionController.java
+++ usecase/src/main/java/com/usecase/controller/TransactionController.java
@@ -1,15 +1,13 @@
 package com.usecase.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
+ 
 import org.springframework.web.bind.annotation.RestController;
-
-import com.usecase.service.TransactionService;
+ 
 
 @RestController
 public class TransactionController{
 
-	@Autowired
-	TransactionService transactionService;
+//	@Autowired
+//	TransactionService transactionService;
 
 	
 }
