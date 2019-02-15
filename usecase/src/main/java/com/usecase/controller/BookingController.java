d8890a2c968f8a97c7cccb6e6c493869d3af5dc0
AyushmanS <AyushmanS@LP-R9030X42.HCLT.CORP.HCL.IN>
2019-02-15 16:55:24 +0530

    As

---
+++ usecase/src/main/java/com/usecase/controller/BookingController.java
@@ -0,0 +1,24 @@
+package com.usecase.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usecase.model.Booking;
+import com.usecase.service.BookingServiceImpl;
+import com.usecase.service.HotelService;
+
+@RestController
+public class BookingController {
+
+	@Autowired
+	BookingServiceImpl bookingService;
+
+	@PostMapping("/booking")
+	public Booking booking(@RequestBody Booking bookRoom) {
+
+		return bookingService.create(bookRoom);
+
+	}
+}
